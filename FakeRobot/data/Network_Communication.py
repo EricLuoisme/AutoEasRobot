92c1c9e5cba4be8331159453e2ce740690164f76
u6270944 <u6270944@anu.edu.au>
2019-01-17 16:27:25 +0800

    update for re-calling sub-script

--- FakeRobot/data/Network_Communication.py
+++ FakeRobot/data/Network_Communication.py
@@ -1,5 +1,5 @@
 # for communication settings
 
-HOST = '127.0.0.100'               # Symbolic name meaning all available interfaces
-PORT = 50007              # Arbitrary non-privileged port
-SUBSCEN_MAXTIME = 180       # Maximum waiting time in sub-scenario
+HOST = '127.0.0.1'               # Symbolic name meaning all available interfaces
+PORT = 8080              # Arbitrary non-privileged port
+SUBSCEN_MAXTIME = 10       # Maximum waiting time in sub-scenario
